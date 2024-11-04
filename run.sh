c0f3bcd7e5170d66903faa20eee77bb55a381ff6
LWang-Laura <leyaowang8@gmail.com>
2024-11-03 19:50:44 -0600

    1103

---
+++ run.sh
@@ -0,0 +1,8 @@
+# CUDA_VISIBLE_DEVICES=0 python Edgeformer-E/main.py --data_path data/Appliances/dropout01_E
+# CUDA_VISIBLE_DEVICES=0 python Edgeformer-E/main.py --data_path data/Appliances/original_E
+# CUDA_VISIBLE_DEVICES=0 python Edgeformer-E/main.py --data_path data/Appliances/knn_E
+# CUDA_VISIBLE_DEVICES=0 python Edgeformer-E/main.py --data_path data/Appliances/E_data #llm
+
+
+# CUDA_VISIBLE_DEVICES=0 python Edgeformer-N/main.py --data_path data/Appliances/original_N
+CUDA_VISIBLE_DEVICES=0 python Edgeformer-N/main.py --data_path data/Appliances/llm_N
